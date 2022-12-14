054140891bc337cae2fc4ca864ca2683ca7f3d5d
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-14 18:18:05 -0300

    adicionando botôes

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Card from '../../components/Card'
 import Header from '../../components/Header'
+import curti from '../../assets/images/curti.png'
+import nao_curti from '../../assets/images/n-curti.png'
 
 function Home() {
   const baseUrlApi =
@@ -13,14 +15,25 @@ function Home() {
   }, [])
   if (!filmes) return null
 
+  const handleClickLeft = () => {
+    console.log('Curtir Clicado :D')
+  }
+  const handleClickRight = () => {
+    console.log('Não curtir Clicado :(')
+  }
+
+  const handleClickCenter = () => {
+    console.log('Pular Clicado :/')
+  }
+
   return (
     <div
       className={`bg-[url('https://image.tmdb.org/t/p/original/198vrF8k7mfQ4FjDJsBmdQcaiyq.jpg')]`}
     >
       <div className={`h-screen bg-opacity-90 bg-red-500`}>
         <Header />
-        <div>
-          <div className="text-center flex flex-col pt-20">
+        <div className="text-center flex flex-col pt-20">
+          <div className="flex flex-col">
             <Card
               key={filmes[0].id}
               titulo={filmes[0].title}
@@ -28,6 +41,27 @@ function Home() {
               imagem={`https://image.tmdb.org/t/p/w500/${filmes[0].backdrop_path}`}
               categoria={filmes[0].media_type}
             />
+
+            <div className="flex justify-center">
+              <button
+                onClick={handleClickLeft}
+                className="bg-white px-10 py-5 text-center rounded-full m-3"
+              >
+                <img src={curti} />
+              </button>
+              <button
+                onClick={handleClickCenter}
+                className="bg-white text-center px-10 rounded-full m-3"
+              >
+                PULAR
+              </button>
+              <button
+                onClick={handleClickRight}
+                className="bg-white text-center px-10 rounded-full m-3"
+              >
+                <img src={nao_curti} />
+              </button>
+            </div>
           </div>
         </div>
       </div>
