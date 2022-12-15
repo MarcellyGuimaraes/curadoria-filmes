6be3da8be4099e84d6f6d1104c9f3bd1ec882374
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-15 13:55:46 -0300

    arrumando css/card/modal

--- src/templates/Curtidos/index.jsx
+++ src/templates/Curtidos/index.jsx
@@ -5,7 +5,7 @@ const Curtidos = ({ movie }) => {
   return (
     <div className="container my-12 mx-auto px-4 md:px-12">
       <p className="text-lg font-bold text-gray-200 text-center">
-        FILMES NÃO CURTIDOS
+        FILMES CURTIDOS
       </p>
       <div className="flex flex-wrap -mx-1 lg:-mx-4">
         {movie?.map((m) => (
